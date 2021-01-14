dc32b40ca632ecc35e74a4e2078a53deb9c5a892
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-01-14 19:32:08 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header'
+import Banner from './components/Banner'
+import Content from './components/Content'
+import CTA from './components/CallToAction'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Content />
+      <CTA />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
